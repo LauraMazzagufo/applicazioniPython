39393b248209733b75cd166ad9d874c64a227763
LauraMazzagufo <79461433+LauraMazzagufo@users.noreply.github.com>
2023-08-05 10:30:36 +0200

    Update guess-the-number-game.py
    version 2 with "random" library

--- guess-the-number-game.py
+++ guess-the-number-game.py
@@ -1,21 +1,33 @@
+import random
+
 print("WELCOME TO THE 🪄  G U E S S  T H E  N U M B E R 🪄  GAME!")
 print()
-print("Please, pick a number between 0 and 1,000,000")
-number = int(input("> "))
+
 counter = 0
-while True:
-  if (number < 556920) and (number > 0):
-    print("The number is too low. Please try again.")
-    counter +=1
-    number = int(input("> "))
-  elif (number > 556920) and (number < 1000000):
-    print("The number is too high. Please try again.")
-    counter +=1
-    number = int(input("> "))
-  elif number==556920:
-    print("You guessed it! 💫 Great job!")
-    break
-  else:
-    print("Did you type a number between 0 and 1000000? Please try again!")
-    number = int(input("> "))
-print("You guessed the number in only", counter, "attempts!")  
+startagain = "run"
+
+while startagain=="run":
+  correct_number = random.randint(1, 100)
+  print("Please, pick a number between 0 and 100")
+  number = int(input("> "))
+  
+  while True:
+    if (number < correct_number) and (number > 0):
+      print("The number is too low. Please try again.")
+      counter +=1
+      number = int(input("> "))
+    elif (number > correct_number) and (number < 100):
+      print("The number is too high. Please try again.")
+      counter +=1
+      number = int(input("> "))
+    elif number==correct_number:
+      print("You guessed it! 💫 Great job!")
+      break
+    else:
+      print("Did you type a number between 0 and 100? Please try again!")
+      number = int(input("> "))
+  print("You guessed the number in only", counter, "attempts!") 
+  print()
+  startagain = input("Type 'run' to try again with a different number. > ")
+  continue
+print("Ok, bye!")
