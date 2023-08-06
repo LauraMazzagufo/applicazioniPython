cb6dab142e7c1644f540e54eab1672268ce6677f
LauraMazzagufo <79461433+LauraMazzagufo@users.noreply.github.com>
2023-08-06 14:12:11 +0200

    Update login-subroutine.py

--- login-subroutine.py
+++ login-subroutine.py
@@ -1,15 +1,14 @@
-print("Replit Login System")
-
 def login():
   while True:
     user = input("What is your username?: >")
     pw = input("What is your password?: >")
     if (user == "david") and (pw == "baldies4life"):
-      print("Welcome to Replit!")
+      print("Welcome, David!")
       break
     else:
       print("Whoops! I don't recognize that username or password. Try again!")
       continue
 
+print("Login System")
 login()
 print(":-)")
