cfce484f54839ac2e0ec7f3a4c8f895f474b6af4
LauraMazzagufo <79461433+LauraMazzagufo@users.noreply.github.com>
2023-08-14 21:05:23 +0200

    Update to-do-list.py

--- to-do-list.py
+++ to-do-list.py
@@ -1,33 +1,59 @@
 #Create your own to do list manager.
 
 import os, time
-print("To Do List Manager")
-myList = []
+list = []
 
-def printList():
-  print() 
-  for item in myList:
-    print(item)
+def view():
   print()
+  counter = 1
+  for i in list:
+    print(counter, f". {i}\n")
+    counter += 1
+
+print("TO DO LIST")
+print()
 
 while True:
-  menu = input("Do you want to view, add, or edit your to do list?: ")
-  if menu == "add" or menu == "Add":
-    item = input("Who should I add to the list?: ")
-    myList.append(item)
-  elif menu == "edit" or menu == "Edit":
-    item = input("What item did you complete?: ")
-    if item in myList:
-      myList.remove(item)
+  menu = input(" Do you want to view, add or remove items? > ").lower() #aggiungo questa funzione per evitare problemi con eventuali lettere maiuscole inserite dall'utente (così add = Add = ADD)
+
+  if menu == "add":
+    item = input("What imput do you want to add? > ")
+    if item in list:
+      print(f"{item} is already in the list!")
     else:
-      print(f"{item} was not in the list")
-  elif menu == "view" or menu == "View":
+      list.append(item)
+      print(f"Item {item} added.")
+    time.sleep(2)
     os.system("clear")
-    print("TO DO LIST:")
-    printList()
-    exit = input("Exit?: ")
-    if exit == "yes":
-       break
+  elif menu == "remove":
+    item = input("What imput do you want to remove? (type 'all' to remove all items) > ")
+    if item in list:
+      check = input(f"Are you sure you want to remove {item}? (y/n) > ")
+      if check == "y" or check == "yes":
+        list.remove(item)
+        print(f"Item {item} cancelled.")
+      elif check == "n" or check == "no":
+        print(f"{item} still present on your list.")
+      time.sleep(2)
+      os.system("clear")
+    elif item == "all":
+      list = []
+      print("All items cancelled.")
+      time.sleep(2)
+      os.system("clear")
+    else:
+      print(f"No item {item} found.")
+      time.sleep(2)
+      os.system("clear")
+  elif menu == "view":
+    view()
+    time.sleep(2)
+    os.system("clear")
+  else:
+    print("Invalid answer.")
+    time.sleep(2)
+    os.system("clear")
+    continue
     else:
       continue
 time.sleep(2)
